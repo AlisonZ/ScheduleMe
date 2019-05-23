58524254e77426cf2e51992eb56d55489c17a272
Alison Zerbe <alisonkzerbe@gmail.com>
2019-05-23 01:07:34 -0700

    refactors header and small text to be a component

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import NavBar from './NavBar';
 import ImageLink from './ImageLink';
+import HeaderAndSmText from './HeaderAndSmText';
 import './styles/HomePage.css';
 
 import acupuncture from './images/acupuncture.jpg';
@@ -11,13 +12,14 @@ import services from './images/services-oil.jpg';
 
 export default class HomePage extends React.Component {
     render() {
+        const header = 'COMMUNITY STICK AND POKE';
+        const text = 'Tincture grass-fed full moon infinite blessings, vitamix tofurkey. Authentic self earth surrender yoni daily kegel practice, impermanent mushrooms. Vinyasa Braggs biodiesel, coconut water kefir. Workshop loving kindness ecstatic dance papasan embodied prius alkaline talking stick, open-minded sunset. Nectar as we honor enlightenment, solstice feline acupuncture popcorn with brewers yeast om raw cacao marijuana perineum discovering valuable truths. Namaste.';
         return(
             <div>
                 <NavBar />
                 <img src={acupuncture} alt="acupuncture" className="homePage-mainImage" />
                 <div>
-                    <h1 className="homePage-textBlurb-title">COMMUNITY STICK AND POKE</h1>
-                    <div className="homePage-textBlub-content">Tincture grass-fed full moon infinite blessings, vitamix tofurkey. Authentic self earth surrender yoni daily kegel practice, impermanent mushrooms. Vinyasa Braggs biodiesel, coconut water kefir. Workshop loving kindness ecstatic dance papasan embodied prius alkaline talking stick, open-minded sunset. Nectar as we honor enlightenment, solstice feline acupuncture popcorn with brewers yeast om raw cacao marijuana perineum discovering valuable truths. Namaste.</div>
+                    <HeaderAndSmText header={header} text={text} />
                 </div>
                 <ul className="homePage-imageList">
                     <li className="homePage-imageItem">
--- src/ServicesPage.js
+++ src/ServicesPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import NavBar from './NavBar';
+import HeaderAndSmText from './HeaderAndSmText';
+import './styles/ServicesPage.css';
+
 export default class ServicesPage extends React.Component {
     render() {
+        const header = 'SERVICES OFFERED';
+        const text = 'Peacock feather kelp prius, master lavender crystalline cold plunge world soul. Reflexology conflict resolution human potential be the change blessed rolfing, holistic mercury retrograde occupy gut flora optimal frequency. What the planet really needs forest jasmine, synchronicity honoring your truth. Farm to table chi energy apple cider vinegar fasting rainbow incense planetary transition, positive affirmation djembe fire tending. Namaste.';
         return(
-            <div>SERVICES PAGE</div>
+            <div>
+                <NavBar />
+                <div className="servicesPage-descr">
+                    <HeaderAndSmText header={header} text={text} />
+                </div>
+            </div>
         );
     }
 }
