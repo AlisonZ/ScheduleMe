2394d8ce94213087490b603c1bb334900f6f4878
Alison Zerbe <alisonkzerbe@gmail.com>
2019-06-03 18:55:16 -0700

    gapi working, need to add sign-in

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import firebase from 'firebase';
 
 import './styles/NavBar.css';
 
 export default class NavBar extends React.Component {
+    state = {
+        user: null
+    }
+
+    componentDidMount() {
+        this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+            (user) => this.setState({user})
+        );
+    }
+
+    componentWillUnmount() {
+        this.unregisterAuthObserver();
+    }
+
+    signOut = () => {
+        firebase.auth().signOut().then(() => {
+            this.props.history.push('/');
+        });
+    }
+
+
     render() {
+
+        const {user} = this.state;
+
+        const userDisplayName = user ? (
+            <strong>
+                {user.displayName || user.phoneNumber}
+            </strong>
+        ) : null;
+        // const signOut = user ? <button onClick={this.signOut}>Sign Out</button>;
+
         return(
             <div>
+                {userDisplayName}
+
                 <ul className="navBar-list">
                     <Link to="/" className="navBar-listItem">Home</Link>
                     <Link to="/about" className="navBar-listItem">About</Link>
--- src/SchedulingPage.js
+++ src/SchedulingPage.js
@@ -1,3 +1,4 @@
+/* global gapi */
 import React from 'react';
 
 import HeaderAndSmText from './HeaderAndSmText';
@@ -5,9 +6,41 @@ import googleCalendar from './images/google-calendar.png';
 
 import './styles/SchedulingPage.css';
 import './App.css';
+import { generateKeyPair } from 'crypto';
 
 
 export default class SchedulingPage extends React.Component {
+    componentDidMount() {
+        let self = this;
+        let scriptTag = document.createElement('script');
+        scriptTag.type = 'text/javascript';
+        scriptTag.src = 'https://apis.google.com/js/api.js';
+
+        scriptTag.addEventListener('load', function(e) {
+            gapi.load('client', () => {
+                console.log('in the load section!!!!');
+                gapi.client.init({
+                    'apiKey': 'AIzaSyDPi0QcVPngFZqWWhfYAhWlnaVSjp1P4tw',
+                    'clientId': '415585169793-csd5jepc693i19l4pj2scj7jc7dcdrlt.apps.googleusercontent.com',
+                    'scope': 'https://www.googleapis.com/auth/calendar',
+                }).then(function() {
+                    // 3. Initialize and make the API request.
+                    return gapi.client.request({
+                    'path': 'https://www.googleapis.com/calendar/v3/calendars/alisonkzerbe@gmail.com',
+                    })
+                }).then(function(response) {
+                    console.log(response.result);
+                }, function(reason) {
+                    console.log('Error: ' + reason.result.error.message);
+                });
+
+            });
+        });
+
+        document.head.appendChild(scriptTag);
+
+    }
+
     render() {
         const header = 'BOOK AN APPOINTMENT';
         const text = 'Mushrooms co-create prayerformance heartbeat of our ancestors juicy downward dog herbal medicine transformative lavender, harmony biomat. Crystalline astral plane gifting circle Big Sur chia seeds ceremonial-grade, toxins vitamin. Folk remedy positive affirmation light energy, ecofriendly bioneers white sage.';
