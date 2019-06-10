1baa10a9d64f9a9ef6bc55b09fcb7a5ce77c77dd
Alison Zerbe <alisonkzerbe@gmail.com>
2019-06-09 20:50:06 -0700

    can add a hard coded event on click from /scheduling. Errors after event and can only add one event for now

--- src/SchedulingPage.js
+++ src/SchedulingPage.js
@@ -1,18 +1,95 @@
 /* global gapi */
 import React from 'react';
+import {Helmet} from 'react-helmet';
 
 import HeaderAndSmText from './HeaderAndSmText';
 import googleCalendar from './images/google-calendar.png';
 import GoogleSignIn from './GoogleSignIn';
+import { CALENDAR_ID, GOOGLE_API_KEY } from "./config.js";
 
 import './styles/SchedulingPage.css';
 import './App.css';
 
 
 export default class SchedulingPage extends React.Component {
-    // state = {
-    //     user: null
-    // }
+   constructor(props) {
+       super(props);
+       this.state = {
+        resource: {
+            "summary": "Appointment",
+            "location": "Somewhere",
+            "start": {
+              "dateTime": "2019-06-16T10:00:00.000-07:00"
+            },
+            "end": {
+              "dateTime": "2019-06-16T10:25:00.000-07:00"
+            }
+          },
+       }
+
+       this.getEvents = this.getEvents.bind(this);
+   }
+
+    getEvents(){
+        let that = this;
+        function start() {
+          gapi.client.init({
+            'apiKey': GOOGLE_API_KEY
+          }).then(function() {
+            return gapi.client.request({
+              'path': `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events`,
+            })
+          }).then( (response) => {
+            let events = response.result.items
+            that.setState({
+              events
+            }, ()=>{
+              console.log(that.state.events);
+            })
+          }, function(reason) {
+            console.log(reason);
+          });
+        }
+        gapi.load('client', start)
+      }
+
+      insertEvents(){
+        let that = this;
+        function start() {
+          gapi.client.init({
+            'apiKey': GOOGLE_API_KEY
+          }).then(function() {
+            return gapi.client.calendar.events.insert({
+                'calendarId': 'primary',
+                'resource': {
+                    "summary": "Appointment",
+                    "location": "Somewhere",
+                    "start": {
+                      "dateTime": "2019-06-16T10:00:00.000-07:00"
+                    },
+                    "end": {
+                      "dateTime": "2019-06-16T10:25:00.000-07:00"
+                    }
+                  }
+              })
+          }).then( (response) => {  
+            response.execute(function(resp) {
+                console.log(resp);
+              })
+            // let events = response.result.items
+            // that.setState({
+            //   events
+            // }, ()=>{
+            //   console.log(that.state.events);
+            // })
+          }, function(reason) {
+            console.log(reason);
+          });
+        }
+        gapi.load('client', start)
+      }
+
+
 
     render() {
         // const {user} = this.state;
@@ -26,8 +103,11 @@ export default class SchedulingPage extends React.Component {
 
         return (
             <div>
+                <Helmet>    
+                    <script src="https://apis.google.com/js/platform.js" async defer></script>
+                </Helmet>
                 <GoogleSignIn />
-                <button onClick={handleButtonClick()}>hiiiiiiiiiiiii</button>
+                <button onClick={this.insertEvents}>hiiiiiiiiiiiii</button>
                 <div className="header-sm-text-desc">
                     <HeaderAndSmText header={header} text={text} />
                     <center>
